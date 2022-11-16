1079a5fb67bb738a4b4a9b215dd77af7752d5a02
v1ab1 <sharkandmouse@gmail.com>
2022-11-17 01:36:01 +0500

    page 12 ready

--- src/pages/Page12/index.jsx
+++ src/pages/Page12/index.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
 import img from "./bg.jpg";
+import closed from "./closed.png";
+import opened from "./opened.png";
+import record from "./record.mp3";
 
-export const Page12 = () => (
-  <div className="page">
-    <img src={img} style={{width: "2160px", height: "1920px"}} alt=""/> 
-  </div>
-);
+export const Page12 = () => {
+  document.addEventListener('keydown', function(event) {
+    if (event.code === 'ArrowRight' || event.code === 'ArrowLeft') {
+      mp3.currentTime = 0;
+      mp3.pause();
+    }
+  });
+  const [handle, setHandle] = useState(false);
+  const mp3 = new Audio(record);
+  if (handle === true) {
+    mp3.play();
+    setHandle(1);
+  };
+  return (
+    <div className="page">
+      <img onClick={() => setHandle(true)} src={handle ? opened : closed} style={styles.image} alt="" />
+      <img src={img} style={{width: "2160px", height: "1920px"}} alt=""/> 
+    </div>
+  )
+};
+
+const styles = {
+  image: {
+    position: "absolute",
+    top: 47,
+    right: 27
+  }
+}
