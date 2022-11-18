d86b4c2aff455056f088e359565f5aa486e03b79
v1ab1 <sharkandmouse@gmail.com>
2022-11-18 21:15:22 +0500

    work on page 10

--- src/pages/Page10/index.jsx
+++ src/pages/Page10/index.jsx
@@ -1,7 +1,52 @@
+import ReactPaint from 'react-paint';
 import img from "./bg.jpg";
+
+export const props = {
+  style: {
+    background: 'tomato',
+    position: "absolute",
+    userDrag: "none",
+    WebkitUserDrag: "none",
+    userSelect: "none",
+    MozUserSelect: "none",
+    WebkitUserSelect: "none",
+    msUserSelect: "none"
+  },
+  brushCol: '#ffffff',
+  lineWidth: 10,
+  className: 'react-paint',
+  height: 500,
+  width: 500,
+  onDraw: () => { console.log('i have drawn!'); },
+};
 
 export const Page10 = () => (
   <div style={{position: "absolute", top: 0}}>
-    <img src={img} style={{width: "2160px", height: "1920px"}} alt=""/> 
+    {/* <ReactPaint {...props} /> */}
+    <canvas style={styles.canvas}>
+
+    </canvas>
+    <img src={img} style={styles.img} alt=""/> 
   </div>
 );
+
+const styles = {
+  img: {
+    width: 2160,
+    height: 1920,
+    userDrag: "none",
+    WebkitUserDrag: "none",
+    userSelect: "none",
+    MozUserSelect: "none",
+    WebkitUserSelect: "none",
+    msUserSelect: "none"
+  },
+  canvas: {
+    position: "absolute",
+    width: 800,
+    height: 1700,
+    background: "white",
+    right: 140,
+    top: 110
+  }
+}
--- src/pages/Page15/index.jsx
+++ src/pages/Page15/index.jsx
@@ -52,10 +52,8 @@ export const Third = () => {
   );
 }
 
-let position;
-
 const MyPreview = () => {
-  const {display, itemType, item, style} = usePreview()
+  const {display, itemType, item, style, ref, monitor} = usePreview()
   if (!display) {
     return null
   }
@@ -66,22 +64,18 @@ const MyPreview = () => {
     return  <img src={second} style={style} alt="" />
   }
   if (item.id === 3) {
-    return  <img className='kek' src={third} style={style} alt="" />
+    console.log(ref)
+    return  <img src={third} style={style} alt="" />
   }
-  console.log(position);
-  position = style;
 }
 
 export const Page15 = () => {
   const [woodCollected, sceneRef] = useDrop(() => ({
     accept: ['first', 'second', 'third'],
-    drop(item) {
+    drop(item, monitor) {
       if (item.id === 3) {
-        const finder = document.getElementsByClassName("kek");
-        const object = document.getElementsByClassName("lol");
-        object[0].style = position;
-      }
-    }
+        console.log(1);
+      }}
   }));
   return (
     <div className="page">
